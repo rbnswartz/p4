601b97c9f42b71f3451cc2ecb7b9d6a768a782ab
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2015-04-27 16:47:27 -0400

    initial commit of p4

---
+++ p4.py
@@ -0,0 +1,26 @@
+#!/bin/python
+import xml.etree.ElementTree as et
+import sys
+import os
+
+def usage():
+    print "Usage: "
+    print "parseprofile.py <file path of PublishProfile file>"
+
+def parse(path):
+    path = os.path.abspath(sys.argv[1])
+    print "Opening " + path + "\n"
+    doc = et.parse(path)
+    root = doc.getroot()
+    element = root.findall("publishProfile[@publishMethod='FTP']")[0]
+    print "Server " + element.attrib["publishUrl"]
+    print "User " + element.attrib["userName"]
+    print "Password " + element.attrib["userPWD"]
+
+def main():
+    if len(sys.argv)> 1:
+        parse(sys.argv[1])
+    else:
+        usage()
+
+main()
